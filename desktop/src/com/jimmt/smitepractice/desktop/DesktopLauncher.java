79d8fe93430e03742c5dffa99b3aa89780a23203
Jimmt <quantum.physics98@gmail.com>
2015-05-28 23:17:25 -0700

    initial commit

---
+++ desktop/src/com/jimmt/smitepractice/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jimmt.smitepractice.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jimmt.smitepractice.SmitePractice;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SmitePractice(), config);
+	}
+}
